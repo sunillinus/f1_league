48bdac543e5aac609a47a2b893f861f2440249a7
sunil <sunil.linus@gmail.com>
2014-03-08 23:41:40 -0800

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+F1League::Application.config.secret_key_base = '9bbed15b848c5df09520109e516df1865c5fbd9330cdcc0bea16617d8eefb664d80bf269dbcae2d84f2fbf282384dfa65874805466d104918ba4647e5d2cbb3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+F1League::Application.config.session_store :cookie_store, key: '_f1_league_session'
